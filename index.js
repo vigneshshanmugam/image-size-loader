b2e269162dd22bcb99aaa634650f8272d5128384
Boopathi Rajaa <me@boopathi.in>
2015-05-06 22:50:15 +0530

    Adds override-able filenames

--- index.js
+++ index.js
@@ -6,23 +6,32 @@ module.exports = function(content) {
 	if(!this.emitFile) throw new Error('emitFile is required from module system');
 
 	this.addDependency(this.resourcePath);
-	var query = loaderUtils.parseQuery(this.query);
 
-	var url = loaderUtils.interpolateName(this, query.name || '[name].[ext]', {
+	var query = loaderUtils.parseQuery(this.query);
+	var filename = "[name].[ext]";
+
+	if (this.options.output.imageFilename) {
+		filename = this.options.output.imageFilename
+	}
+	// query.name overrides imageFilename
+	if ('string' === typeof query.name) {
+		filename = query.name;
+	}
+
+	var url = loaderUtils.interpolateName(this, filename, {
 		context: query.context || this.options.context,
 		content: content,
 		regExp: query.regExp
 	});
 
-	dimensions = getDimensions(this.resourcePath);
+	var dimensions = getDimensions(this.resourcePath);
 
-	var publicPath = "";
-	if(this.options.output.publicPath) publicPath = this.options.output.publicPath;
-	dimensions.src = publicPath + url;
-	dimensions.geometry = dimensions.width + "x" + dimensions.height;
+	dimensions.src = this.options.output.publicPath
+		? this.options.output.publicPath + url
+		: url;
 
 	this.emitFile(url, content);
-	return "module.exports = " + JSON.stringify(dimensions);
+	return 'module.exports = ' + JSON.stringify(dimensions);
 };
 
 module.exports.raw = true;
